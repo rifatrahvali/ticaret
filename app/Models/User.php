748e0d0009fb6eeb9a5460ddfa61287db8b8c610
RIFAT RAHVALI <rifatrahvali@outlook.com>
2024-05-24 00:10:51 +0300

    notification işlemi

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    // Notifiable : hangi model treadi varsa onla gönderebiliriz.
 
     /**
      * The attributes that are mass assignable.
